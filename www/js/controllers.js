8f25f439b28002f66652b35a8d1ed4ff58ed04d9
Rodrigo Bournissent <rodrigobourni@hotmail.com>
2016-05-14 14:18:48 -0300

    Add dynamic data to Contactos and Chats tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,54 @@
 angular.module('app.controllers', [])
-     
+
 .controller('chatsCtrl', function($scope) {
-
+  $scope.conversations = [{
+    name: 'Juan',
+    avatar_url: 'http://pickaface.net/includes/themes/clean/img/slide4.png',
+    connected: true,
+    writing: true,
+    last_message: new Date('2016-05-14 12:23:25')
+  }, {
+    name: 'Jorge',
+    avatar_url: 'http://www.interdating-ukrainian-women.com/wp-content/uploads/2013/06/avatar.jpg',
+    connected: true,
+    writing: false,
+    last_message: new Date('2016-05-12 12:23:25')
+  }, {
+    name: 'Lucía',
+    avatar_url: 'https://www.tm-town.com/assets/default_female600x600-3702af30bd630e7b0fa62af75cd2e67c.png',
+    connected: false,
+    writing: false,
+    last_message: new Date('2016-05-08 12:23:25')
+  }];
 })
-   
+
 .controller('contactosCtrl', function($scope) {
-
+  $scope.contacts = [{
+    name: 'Juan',
+    avatar_url: 'http://pickaface.net/includes/themes/clean/img/slide4.png',
+    connected: true,
+    last_connection: new Date('2016-05-14 12:23:25')
+  }, {
+    name: 'Jorge',
+    avatar_url: 'http://www.interdating-ukrainian-women.com/wp-content/uploads/2013/06/avatar.jpg',
+    connected: true,
+    last_connection: new Date('2016-05-14 12:23:25')
+  }, {
+    name: 'Lucía',
+    avatar_url: 'https://www.tm-town.com/assets/default_female600x600-3702af30bd630e7b0fa62af75cd2e67c.png',
+    connected: false,
+    last_connection: new Date('2016-05-10 12:23:25')
+  }];
 })
-   
+
 .controller('juanDetallesCtrl', function($scope) {
 
 })
-   
+
 .controller('juanConversaciNCtrl', function($scope) {
 
 })
-   
+
 .controller('settingsCtrl', function($scope) {
 
 })
- 
